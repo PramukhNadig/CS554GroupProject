a778cda34077ba01cf8613b4acb84258f3ca5bf2
moar <mmashiac@stevens.edu>
2023-05-07 05:03:23 +0500

    Update MySets.tsx

--- client/src/pages/MySets.tsx
+++ client/src/pages/MySets.tsx
@@ -1,6 +1,9 @@
+/** @format */
+
 import Set from "../components/Set";
 import axios from "axios";
 import { useQuery } from "react-query";
+import { Box, Grid } from "@mui/material";
 
 function App() {
   const { data } = useQuery(["MySets"], () => {
@@ -10,18 +13,21 @@ function App() {
   });
   console.log("data", data);
   return (
-    <div className="app">
-      {data?.map((v: any) => {
-        return (
-          <Set
-            subject={v?.subject}
-            title={v?.title}
-            description={v?.description}
-            cards={v?.cards}
-          ></Set>
-        );
-      })}
-    </div>
+    <Box sx={{ padding: 2 }}>
+      <Grid container spacing={2}>
+        {data?.map((v: any) => {
+          return (
+            <Grid item xs={12} sm={6} md={4} key={v.id}>
+              <Set
+                subject={v?.subject}
+                title={v?.title}
+                description={v?.description}
+                cards={v?.cards}></Set>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 }
 
