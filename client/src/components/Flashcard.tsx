2f6261c2dab019c4910342eeaa997098df1def86
Maor Mashiach <maormashiach@Maors-MacBook-Pro-2.local>
2023-05-09 21:21:24 -0400

    preparing flashcard for finish

---
+++ client/src/components/Flashcard.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+import { useState } from "react";
+import { Card, Button, ListGroup, Container } from "react-bootstrap";
+import axios from "axios";
+import cookies from "../helpers/cookies";
+import { useQuery } from "react-query";
+
+function App({ subject, title, description, cards, setId, owner }: any) {
+  const [index, setIndex] = useState(0);
+  const [showBack, setShowBack] = useState(false);
+  const currentCard = cards?.[index];
+
+  const handleClick = () => {
+    setShowBack(!showBack);
+  };
+
+  const { data: savedSets } = useQuery(["SavedSets"], () => {
+    if (!cookies.doesExist("username")) return [];
+    return axios
+      .get(
+        "http://localhost:4000/v1/sets/saved/" + cookies.getCookie("username")
+      )
+      .then((res) => {
+        return res.data.saved_sets;
+      });
+  });
+
+  const nav = (own: any) => {
+    return "/userprofile/" + own;
+  };
+
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Card style={{ width: "48rem" }}>
+        <Card.Img variant="top" src={currentCard?.imageUrl} />
+        <Card.Body>
+          <div className="app">
+            <Button
+              variant="contained"
+              onClick={() => {
+                if (cookies.doesExist("username")) {
+                  if (savedSets?.some((set: any) => set === setId)) {
+                    axios.post("http://localhost:4000/v1/sets/save", {
+                      username: cookies.getCookie("username"),
+                      setId,
+                    });
+                  } else {
+                    axios.post("http://localhost:4000/v1/sets/unsave", {
+                      username: cookies.getCookie("username"),
+                      setId,
+                    });
+                  }
+                }
+              }}
+            >
+              {/* {savedSets?.some((set: any) => set === setId)
+                ? "Unsave"
+                : "Save"} */}
+            </Button>
+            {/* <Card.Title>
+              <a href={nav(owner)}>{"Owner: " + owner}</a>
+            </Card.Title> */}
+            {/* <Card.Title>{"Subject: " + subject}</Card.Title>
+            <Card.Title>{"Title: " + title}</Card.Title> */}
+            {/* <Card.Subtitle className="mb-2 text-muted">
+              {"Description: " + description}
+            </Card.Subtitle> */}
+            <div className="card">
+              {currentCard?.imageUrl && currentCard?.imageUrl !== "" && (
+                <img
+                  src={currentCard?.imageUrl}
+                  alt={currentCard?.word}
+                  style={{ maxWidth: "100%" }}
+                />
+              )}
+              <div className="card-details">
+                <h3>{currentCard?.word}</h3>
+                <p>{showBack && currentCard?.meaning}</p>
+              </div>
+              <Button onClick={handleClick} style={{ marginTop: 2 }}>
+                Flip
+              </Button>
+            </div>
+            <div
+              className="card-nav"
+              style={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Button
+                variant="contained"
+                disabled={index === 0}
+                onClick={() => {
+                  setIndex(index - 1);
+                  setShowBack(false);
+                }}
+              >
+                Prev
+              </Button>
+              <Button
+                variant="contained"
+                disabled={index === cards?.length - 1}
+                onClick={() => {
+                  setIndex(index + 1);
+                  setShowBack(false);
+                }}
+              >
+                Next
+              </Button>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
+
+export default App;
