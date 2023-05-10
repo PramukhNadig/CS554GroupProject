fcc27c286cf4547d3f036d6a60310e9145604919
Maor <mmashiac@stevens.edu>
2023-05-09 23:39:47 -0400

    fixed the issue with centering flashcard

--- client/src/components/Flashcard.tsx
+++ client/src/components/Flashcard.tsx
@@ -18,8 +18,6 @@ function App({ subject, title, description, cards, setId, owner }: any) {
   const [showBack, setShowBack] = useState(false);
   const currentCard = cards?.[index];
 
-
-
   const handleClick = () => {
     setShowBack(!showBack);
   };
@@ -48,97 +46,16 @@ function App({ subject, title, description, cards, setId, owner }: any) {
   }));
 }
 
-  // return (
-  //   <Container
-  //     className="d-flex justify-content-center align-items-center"
-  //     style={{ minHeight: "100vh" }}
-  //   >
-  //     <Card style={{ width: "48rem" }}>
-  //       <Card.Img variant="top" src={currentCard?.imageUrl} />
-  //       <Card.Body>
-  //         <div className="app">
-  //           <Button
-  //             variant="contained"
-  //             onClick={() => {
-  //               if (cookies.doesExist("username")) {
-  //                 if (savedSets?.some((set: any) => set === setId)) {
-  //                   axios.post("http://localhost:4000/v1/sets/save", {
-  //                     username: cookies.getCookie("username"),
-  //                     setId,
-  //                   });
-  //                 } else {
-  //                   axios.post("http://localhost:4000/v1/sets/unsave", {
-  //                     username: cookies.getCookie("username"),
-  //                     setId,
-  //                   });
-  //                 }
-  //               }
-  //             }}
-  //           >
-  //             {/* {savedSets?.some((set: any) => set === setId)
-  //               ? "Unsave"
-  //               : "Save"} */}
-  //           </Button>
-  //           {/* <Card.Title>
-  //             <a href={nav(owner)}>{"Owner: " + owner}</a>
-  //           </Card.Title> */}
-  //           {/* <Card.Title>{"Subject: " + subject}</Card.Title>
-  //           <Card.Title>{"Title: " + title}</Card.Title> */}
-  //           {/* <Card.Subtitle className="mb-2 text-muted">
-  //             {"Description: " + description}
-  //           </Card.Subtitle> */}
-  //           <div className="card">
-  //             {currentCard?.imageUrl && currentCard?.imageUrl !== "" && (
-  //               <img
-  //                 src={currentCard?.imageUrl}
-  //                 alt={currentCard?.word}
-  //                 style={{ maxWidth: "100%" }}
-  //               />
-  //             )}
-  //             <div className="card-details">
-  //               <h3>{currentCard?.word}</h3>
-  //               <p>{showBack && currentCard?.meaning}</p>
-  //             </div>
-  //             <Button onClick={handleClick} style={{ marginTop: 2 }}>
-  //               Flip
-  //             </Button>
-  //           </div>
-  //           <div
-  //             className="card-nav"
-  //             style={{ display: "flex", justifyContent: "space-between" }}
-  //           >
-  //             <Button
-  //               variant="contained"
-  //               disabled={index === 0}
-  //               onClick={() => {
-  //                 setIndex(index - 1);
-  //                 setShowBack(false);
-  //               }}
-  //             >
-  //               Prev
-  //             </Button>
-  //             <Button
-  //               variant="contained"
-  //               disabled={index === cards?.length - 1}
-  //               onClick={() => {
-  //                 setIndex(index + 1);
-  //                 setShowBack(false);
-  //               }}
-  //             >
-  //               Next
-  //             </Button>
-  //           </div>
-  //         </div>
-  //       </Card.Body>
-  //     </Card>
-  //   </Container>
-  // );
-
   return (
-    <div>
-      <FlashcardArray cards={cardList} />
-    </div>
-  )
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div style={{ width: "48rem" }}>
+        <FlashcardArray cards={cardList} />
+      </div>
+    </Container>
+  );
 }
 
 export default App;
