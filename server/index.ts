7af802827ec587b1f401b8bb283d811312d2eac8
Your Name <you@example.com>
2023-05-10 13:12:58 -0400

    Procfile changed

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ import { connectRedis } from "./config/redis.js";
 dotenv.config();
 
 const app: Express = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
