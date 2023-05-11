59935e4848d952e6dd70016d77013a0595f6d41c
PramukhNadig <72940884+PramukhNadig@users.noreply.github.com>
2023-05-10 22:01:19 -0400

    Update assistant.ts

--- server/services/assistant.ts
+++ server/services/assistant.ts
@@ -5,7 +5,7 @@ dotenv.config();
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 
